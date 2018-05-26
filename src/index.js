e4784ec41121055a7d8a6ef3aad19f7765d57ad5
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2018-05-26 19:04:31 +0700

    Use grid in app

--- src/index.js
+++ src/index.js
@@ -5,9 +5,26 @@ import {render} from 'react-dom';
 
 type Props = {};
 
+let styles = {
+  wrapper: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridGap: 10,
+  },
+  item: {
+    backgroundColor: 'lightblue',
+  },
+};
+
 class App extends Component<Props> {
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div style={styles.wrapper}>
+        <div style={styles.item}>Hello</div>
+        <div style={styles.item}>World</div>
+        <div style={styles.item}>!</div>
+      </div>
+    );
   }
 }
 
