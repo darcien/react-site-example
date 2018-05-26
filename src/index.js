6a01d1abfcdc895a6001969907f0c3398e723366
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2018-05-26 17:56:02 +0700

    Add react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import "./style.scss";
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 
-console.log("Hello world!");
+class App extends Component {
+  render() {
+    return <div>'Hello world!'</div>;
+  }
+}
+
+render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 // Config for webpack v4
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackMd5Hash = require("webpack-md5-hash");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: { main: "./src/index.js" },
+  entry: {main: './src/index.js'},
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
   },
   module: {
     rules: [
@@ -18,31 +17,18 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
       },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin("dist", {}),
-    new MiniCssExtractPlugin({
-      filename: "style.[contenthash].css",
-    }),
+    new CleanWebpackPlugin('dist', {}),
     new HtmlWebpackPlugin({
       inject: false,
       hash: true,
-      template: "./src/index.html",
-      filename: "index.html",
+      template: './src/index.html',
+      filename: 'index.html',
     }),
     new WebpackMd5Hash(),
   ],
